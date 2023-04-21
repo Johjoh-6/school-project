d8753e15f5962cae02d38b40890fe9d745c1347b
Johann six <six.johann@gmail.com>
2023-04-21 15:07:01 +0200

    🔧 add config

--- web_app/app.vue
+++ web_app/app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+ <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 max-w-screen-xl flex items-center justify-end p-4 w-full gap-4">
+    <NuxtLink to="/" class="n-link-base ">
+      Home
+    </NuxtLink>
+  </nav>
+  <NuxtLayout >
+    <NuxtPage  />
+  </NuxtLayout>
 </template>
